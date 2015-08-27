ac297a41a05c6d6258523c69a00b65a6f64293d8
Mark Thomas <markbt@efaref.net>
2015-08-27 16:25:02 +0100

    Initial commit

---
+++ AssetIcons/Identity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using ICities;
+
+namespace AssetIcons
+{
+    public class Identity : IUserMod
+    {
+        public string Name
+        {
+            get { return "AssetIcons"; }
+        }
+
+        public string Description
+        {
+            get { return "Adds icons to assets based on their thumbnails."; }
+        }
+    }
+}
+
